a43318400ea59c23cc34148a5846e91aa7bd9304
Neil <neil.mi0xzz@gmail.com>
2020-05-11 11:02:03 +0100

    updated logging config constants

---
+++ wsjtx-dx-alert/config.py
@@ -0,0 +1,8 @@
+import yaml
+import logging
+
+LOG_LEVEL = logging.DEBUG
+LOG_FILE = '/tmp/wsjtx-dx-alert.log'
+
+with open('settings.yaml', 'r') as f:
+    settings = yaml.load(f, Loader=yaml.FullLoader)
